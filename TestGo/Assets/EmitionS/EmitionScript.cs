5d10321be306b91f885e0762af0e25a595a2f5ad
Miguel Pereira <167128047+Kencor2019@users.noreply.github.com>
2025-09-16 22:19:22 -0300

    toin
    Adcionados Menu inicial com coisas legais (mas eu não sei se ta legal de vdd alguem tem q ver né)
    Falta uma tela de loading pra trocar scenes (porque é bonitinho)
    Talvez eu adcione um multiplayer (se tudo correr bem)
    Mas prioridade agora é desenvolver uma arvore tecnologica e tela loading (dps desenvolveremos o jogo)

---
+++ TestGo/Assets/EmitionS/EmitionScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EmitionScript : ScriptableObject
+{
+
+    [SerializeField] private int municaoMax;
+    [SerializeField] private GameObject balaOrigem;
+    private GameObject[] bala;
+    private int municao;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        municao = 0;
+        bala = new GameObject[municaoMax];
+        for (int i = 0; i < municaoMax; i++)
+        {
+            bala[i] = Instantiate(balaOrigem);
+            bala[i].SetActive(false);
+        }
+    }
+
+    public GameObject instanciar()
+    {
+        bala[municao % municaoMax].SetActive(true);
+
+        municao++;
+
+        return bala[municao % municaoMax];
+    }
+
+    public GameObject instanciar(Vector3 position, Quaternion rotation)
+    {
+        bala[municao % municaoMax].SetActive(true);
+        bala[municao % municaoMax].transform.position = position;
+        bala[municao % municaoMax].transform.rotation = rotation;
+
+        municao++;
+
+        return bala[municao % municaoMax];
+    }
+
+    public GameObject[] getBalas()
+    {
+        return bala;
+    }
+
+    public int getMunicao()
+    {
+        return municao % municaoMax;
+    }
+
+    public int getMunicaoMax()
+    {
+        return municaoMax;
+    }
+    
+}
---
+++ TestGo/Assets/ManuScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private EmitionScript olhoBizarro;
+    [SerializeField] private EmitionScript Bizarro;
+    private float counter;
+    private float limit = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        olhoBizarro.Start();
+        //Bizarro.Start();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        olhosBizarros(0);
+    }
+
+    bool olhosBizarros(float antibug)
+    {
+        //Contador padrão
+        counter += Time.deltaTime;
+        if (counter < limit)
+        {
+            return false;
+        }
+
+        Vector3 pos = new Vector3(UnityEngine.Random.Range(-8.5f - antibug, 8.5f + antibug), UnityEngine.Random.Range(-5 - antibug, 5 + antibug), 0);
+
+        foreach (GameObject bullet in olhoBizarro.getBalas())
+        {
+            if (Vector3.Distance(pos, bullet.transform.position) < 3.5f) return olhosBizarros(antibug += 0.05f);
+        }
+        //Esse acima garante q não vai ter nenhum em cima do outro e o antibug é pra caso o espaço da cam não seja bastante(evita stackoverflow)
+
+        //"invoca" o fundinho
+        olhoBizarro.instanciar(pos, Quaternion.identity);
+
+        //faz a primeira carinha ter sorriso feio(feio pq ta mal desenhado) só q ficou feio então tirei
+        //if (olhoBizarro.getMunicao() == 0) Bizarro.instanciar(pos - Vector3.up / 2, Quaternion.identity);
+
+        //reseta o contador e calcula aleatoriamente quanto tempo vai ter o proximo pra aparecer
+        counter = 0;
+        limit = UnityEngine.Random.Range(0.6f, 1.7f);
+
+        return true;
+
+    }
+
+    public void playButton()
+    {
+        //preguiça de fazer um especifico pra isso mas acho q vo fzr dps(botão pra cmcr o jogo o.O)
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+}
