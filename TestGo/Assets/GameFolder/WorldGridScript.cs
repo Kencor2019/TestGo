41cb7490718da6c84cf29b162ec5f793cb3b14f6
Miguel Pereira <167128047+Kencor2019@users.noreply.github.com>
2025-09-28 21:10:29 -0300

    MundoPronto
    Criei uma script pra fazer nosso querido mundinho, ficou simples e top, agora o meu plano é fazer codigo pra pegar qual o chão do grid(pra brocas e fabricas), posicionar o mundo certinho,  fazer o codigo de posicionar as fabricas, fazer o player e fazer as coisas q as fabricas fazem (tbm tem as torretas), vou começar fazendo as fabricas provavelmente porque me parece mais legal no momento, tbm tenho q por o UI o.O, dor no peito hoje foda, boa noite a todos irei Khazikar (amo o KhaZix)

--- TestGo/Assets/GameFolder/WorldGridScript.cs
+++ TestGo/Assets/GameFolder/WorldGridScript.cs
@@ -5,16 +5,81 @@ using UnityEngine;
 public class WorldGridScript : MonoBehaviour
 {
     [SerializeField]private Vector3 worldSize;
-    private int[][] world;
+    private Color[] _world;
     // Start is called before the first frame update
     void Start()
     {
-        world = new int[(int)worldSize.x][];
+        startMap();   
+    }
+
+    void startMap()
+    {
+        _world = new Color[(int)worldSize.x * (int)worldSize.y];
+
+        //prepara as cores pro mapa
+        getNoise();
+
+        //textura q vai receber cada pixel do mapa (ela recebendo e depois o apply)
+        Texture2D _texture = new Texture2D((int)worldSize.x, (int)worldSize.y, TextureFormat.RGBA32, false);
+
+        // Desliga o filtro bilinear (usa point filtering)
+        _texture.filterMode = FilterMode.Point;
+
+        // Opcional: evita wrap (repetição da textura)
+        _texture.wrapMode = TextureWrapMode.Clamp;
+
+        //Recebe os pixels e da o apply
+        _texture.SetPixels(0, 0, (int)worldSize.x, (int)worldSize.y, _world);
+
+        _texture.Apply();
+
+        //o sprite pra rendereziar nossa textura q é o nosso mapa
+        Sprite spr = Sprite.Create(_texture, new Rect(0.0f, 0.0f, _texture.width, _texture.height), Vector2.zero, 1f);
+        this.GetComponent<SpriteRenderer>().sprite = spr;
+    }
+
+    void getNoise()
+    {
         for (int i = 0; i < worldSize.x; i++)
         {
-            world[i] = new int[(int)worldSize.y];
+            for (int d = 0; d < worldSize.y; d++)
+            {
+                float xCoord = ((float)i / 250f) * 5;
+                float yCoord = ((float)d / 250f) * 5;
+
+                float diggo = Mathf.PerlinNoise(xCoord, yCoord);
+
+                Debug.Log(_world.Length + " : " + diggo);
+
+                _world[i * (int)worldSize.y + d] = new Color(0.7411765f, 0.7176471f, 0.4196079f, 1f);
+
+                //pontos altos = branco
+                if (diggo > 0.7f)
+                {
+                    _world[i * (int)worldSize.y + d] = new Color(1f, 0.937255f, 0.8352942f, 1f);
+                } else
+                //pontos meio altos = marrom
+                if (diggo > 0.65f)
+                {
+                    _world[i * (int)worldSize.y + d] = new Color(0.9568628f, 0.6431373f, 0.3764706f, 1f);
+                } else
+                //terreno normal = verde
+                if (diggo > 0.40f)
+                {
+                    _world[i * (int)worldSize.y + d] = new Color(0.5960785f, 0.9843138f, 0.5960785f, 1f);
+                } else
+                //terreno baixo = amarelo
+                if (diggo > 0.37f)
+                {
+                    _world[i * (int)worldSize.y + d] = new Color(0.9333334f, 0.909804f, 0.6666667f, 1f);
+                } else
+                //terreno megabaixo = azul
+                if (diggo > 0.01f)
+                {
+                    _world[i * (int)worldSize.y + d] = new Color(0.5294118f, 0.8078432f, 0.9215687f, 1f);
+                }
+            }
         }
-        
     }
 
     // Update is called once per frame
